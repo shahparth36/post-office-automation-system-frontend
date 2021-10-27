17f4e2dc0e60e60f3eae696bdc23dededd191465
shahparth36 <parthshah1936@gmail.com>
2021-10-28 02:31:09 +0530

    changed baseURL

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://post-office-automation-api/api",
 });
 
 instance.interceptors.request.use(
